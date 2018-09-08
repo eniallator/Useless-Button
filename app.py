7347aa1ad6b5c8982f8e99748ef465babfd02734
eniallator <eniallatormc@gmail.com>
2018-09-08 18:10:48 +0100

    Made the client talk with the server

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-import flask
+from flask import Flask, request, send_from_directory
 from os import path
 
-APP = flask.Flask(__name__, static_folder='/static',static_url_path='/static')
+APP = Flask(__name__, static_folder='/static',static_url_path='/static')
 
-@APP.route('/')
+@APP.route('/', methods=['GET'])
 def index():
-    return flask.send_from_directory('static', 'index.html')
+    return send_from_directory('static', 'index.html')
+
+@APP.route('/', methods=['POST'])
+def buttonClicked():
+    if request.mimetype == 'application/json':
+        print(request.json)
+    return 'Received request'
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=3000, debug=True)
