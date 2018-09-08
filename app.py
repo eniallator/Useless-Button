96c1b87d1ea41fbdbf2df3714050355dab66706f
eniallator <eniallatormc@gmail.com>
2018-09-08 19:03:25 +0100

    Got server side communication working, implemented it using sockets

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 from flask import Flask, request, send_from_directory
+from flask_socketio import SocketIO, emit
+import json
+from re import match
 from os import path
 
-APP = Flask(__name__, static_folder='/static',static_url_path='/static')
+APP = Flask(__name__, static_folder='/static', static_url_path='/static')
+SOCKETIO = SocketIO(APP)
+
+USELESS_LOGS = ['App started']
 
 @APP.route('/', methods=['GET'])
 def index():
     return send_from_directory('static', 'index.html')
 
-@APP.route('/', methods=['POST'])
-def buttonClicked():
-    if request.mimetype == 'application/json':
-        print(request.json)
-    return 'Received request'
+@SOCKETIO.on('login')
+def login():
+    emit('logs update', '\n'.join(USELESS_LOGS))
+
+@SOCKETIO.on('button click')
+def buttonClicked(msg):
+    jsonMsg = json.loads(msg)
+
+    if 'userName' not in jsonMsg or not match(r'^\w+$', jsonMsg['userName']) or len(jsonMsg['userName']) > 20:
+        return
+
+    USELESS_LOGS.append(jsonMsg['userName'] + ' pressed the button!')
+
+    if len(USELESS_LOGS) > 25:
+        USELESS_LOGS.pop(0)
+
+    emit('logs update', '\n'.join(USELESS_LOGS), broadcast=True)
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=3000, debug=True)
+    # APP.run(host='0.0.0.0', port=3000, debug=True)
+    SOCKETIO.run(APP, host='0.0.0.0', port=3000)
